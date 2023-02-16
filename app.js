03bfc57fc2f075615ff10ae543134dcedb2385df
Maxwell Ahmadu <ahmadmax046@gmail.com>
2023-02-16 05:47:28 +0100

    added views

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const rateLimit = require("express-rate-limit")
+const { requiresAuth } = require('express-openid-connect')
 const helmet = require('helmet')
 const logger = require("./logging/logger")
 
@@ -7,6 +8,9 @@ const config = require('./config/config')
 const booksRoute = require('./routes/books')
 const authorsRoute = require('./routes/authors')
 const connectToDB = require('./database/db')
+const Books = require("./models/booksModel");
+
+const auth0Middleware = require('./auth/auth0');
 
 const app = express()
 
@@ -16,6 +20,11 @@ app.use(express.json())
 
 //Connect to DB
 connectToDB()
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+app.use(auth0Middleware)
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
@@ -32,6 +41,27 @@ app.use(helmet())
 app.use('/api/v1/books', booksRoute)
 app.use('/api/v1/authors', authorsRoute)
 
+app.get('/', (req, res) => {
+    res.render('index', {
+         user: req.oidc.user,
+    });
+});
+
+//Get profile
+// Add the requiresAuth middleware for routes that require authentication
+app.get('/profile',requiresAuth(), (req, res) => {
+    //console.log(req.oidc.user);
+    res.render('profile', {
+        user: req.oidc.user,
+        Books
+    });
+});
+
+app.post('/logout', (req, res) => {
+    req.oidcLogout();
+    res.redirect('/');
+});
+
 // error handler
 app.use((err, req, res, next) => {
     const errorStatus = err.status || 500
@@ -40,5 +70,5 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(config.PORT, () => {
-    logger.info(`App is runing on ${config.PORT}`)
+    logger.info(`App is runing on Port ${config.PORT}`)
 })
